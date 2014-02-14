0c63c7cbfbe951307750cd9537a43dc83e36cf31
Ester Ytterbrink <ester.ytterbrink@jayway.com>
2014-02-14 10:48:22 +0100

    File backup from %Y%m%d%H%M

---
+++ backupConfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+filename="files.txt"
+while read line
+do
+	name=$line
+	echo "Text read from file - $name" 
+	eval cp $name .
+	git add *
+	git commit -m "File backup from %Y%m%d%H%M"
+done<$filename	
+
+
