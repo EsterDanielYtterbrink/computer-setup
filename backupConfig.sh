5a35cb60cd26dd550de9f4164c67773615c4a092
Ester Ytterbrink <ester.ytterbrink@jayway.com>
2014-02-19 14:03:25 +0100

    File backup from 201402191403

--- backupConfig.sh
+++ backupConfig.sh
@@ -6,7 +6,7 @@ do
 	name=$line
 	echo "Text read from file - $name"
 	echo "Debug" + $(eval ls -la "configurefiles/") 
-	eval cp $name "configurefiles/"
+	eval cp -r  $name "configurefiles/"
 done<$filename	
 git add *
 git commit -m "File backup from $(date +'%Y%m%d%H%M')"	
