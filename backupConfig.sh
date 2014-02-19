f9091e2f97c775dde7286211c3674ec4c6bb3130
Ester Ytterbrink <ester@Esters-MacBook-Pro.local>
2014-02-19 11:07:23 +0100

    File backup from 201402191107

--- backupConfig.sh
+++ backupConfig.sh
@@ -5,7 +5,7 @@ while read line
 do
 	name=$line
 	echo "Text read from file - $name" 
-	eval cp $name .
+	eval cp $name ./configurefiles
 	git add *
 	git add .*
 	git commit -m "File backup from $(date +'%Y%m%d%H%M')"
