279b881905db7659ecfd8f8d1e8281a2010d9fd0
Ester Ytterbrink <ester.ytterbrink@jayway.com>
2014-02-19 12:36:19 +0100

    Tests filebackup

--- backupConfig.sh
+++ backupConfig.sh
@@ -4,11 +4,10 @@ filename="files.txt"
 while read line
 do
 	name=$line
-	echo "Text read from file - $name" 
-	eval cp $name ./configurefiles/
-	git add *
-	git add .*
-	git commit -m "File backup from $(date +'%Y%m%d%H%M')"
+	echo "Text read from file - $name"
+	echo "Debug" + $(eval ls -la "configurefiles/") 
+	eval cp $name "configurefiles/"
 done<$filename	
-
+git add *
+git commit -m "File backup from $(date +'%Y%m%d%H%M')"	
 
